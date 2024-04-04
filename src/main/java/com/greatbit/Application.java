1fe9cb39b7ffe7a3b93184ca9876df81fb590b49
Dmitry-Frantsuzov <Frantsyz321@mail.ru>
2024-04-04 23:18:45 +0700

    Created a simplified pattern CRUD

--- src/main/java/com/greatbit/Application.java
+++ src/main/java/com/greatbit/Application.java
@@ -1,2 +1,16 @@
-package com.greatbit;public class Application {
+package com.greatbit;
+
+
+import com.greatbit.models.Book;
+import com.greatbit.models.BooksStorage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);  //запуск приложения на Spring Boot
+    }
+
 }
--- src/main/java/com/greatbit/models/BooksStorage.java
+++ src/main/java/com/greatbit/models/BooksStorage.java
@@ -1,2 +1,28 @@
-package com.greatbit.models;public class BooksStorage {
+package com.greatbit.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class BooksStorage {
+    private static List<Book> books = new ArrayList<>();
+
+    static {
+        books.add(new Book(
+                UUID.randomUUID().toString(),
+                "Учение Дона Хуана",
+                "Карлос Кастанеда",
+                400));
+        books.add(new Book(
+                UUID.randomUUID().toString(),
+                "Богатый папа, Бедный папа",
+                "Роберт Киосаки",
+                300));
+    }
+
+    public static List<Book> getBooks() {
+        return books;
+    }
+
+
 }
